429e7cea4313f78ee8cf5a7dfcba74bc2d62b270
gv0413 <ggvv0413@gmail.com>
2025-02-22 20:37:06 +0900

    header, footer 생성

---
+++ vipwave/src/lib/components/footer/Footer.tsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <footer className="mt-8 sticky top-[100vh] w-full text-justify flex justify-between text-zinc-600 px-8 py-4 border-t border-solid border-t-gray-600">
+      <div>
+        <p>VIPWAVE</p>
+        <p>© 빅뱅음원총공팀</p>
+      </div>
+      <div>
+        <p>instagram @bigbangmusic0819</p>
+        <p>문의 bbmusicattack@gmail.com</p>
+      </div>
+    </footer>
+  );
+}
