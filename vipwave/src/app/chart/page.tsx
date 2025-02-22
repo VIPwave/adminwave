7bb7712917d49728504d53ec5b5272e3b4914ab6
dino <youheedong@gmail.com>
2025-02-22 23:37:19 +0900

    fix: deploy

--- vipwave/src/app/chart/page.tsx
+++ vipwave/src/app/chart/page.tsx
@@ -1,39 +1,26 @@
 import Charts from "@/lib/components/chart/Charts";
-import { Song } from "@/lib/components/chart/ChartTable";
+import { loadPosts } from "@/lib/load-charts";
 import { convertRedableChartType } from "@/lib/utils";
-import dayjs from "dayjs";
-import { MongoClient } from "mongodb";
 
-type ChartAgreegation = {
-  agreegation: Array<ChartItem>;
-};
+async function fetchData() {
+  try {
+    const result = await loadPosts();
+    return result
+  } catch(e) {
+    console.log("err= ", e)
+  }
+}
 
-type ChartItem = {
-  platform: string;
-  type: string;
-  items: Array<Song>;
-  timestamp: string;
-};
 
 export default async function ChartPage() {
-  // TODO: 유틸성 method 분리 및 정리
-  const client = new MongoClient(
-    "mongodb+srv://service_readonly_db_user:8sGfklmdfABM72Cd@cluster0.lruc9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-  );
-  await client.connect();
-  const db = client.db("music_charts");
-  const collection = db.collection("charts");
-  // NOTE: mongoDB의 Data이기 때문에, 임의로 캐스팅함.
-  const result: ChartAgreegation = (await collection.findOne({
-    timestamp: dayjs().subtract(10, "m").format("YYYY.MM.DD HH:00"),
-  })) as unknown as ChartAgreegation;
+  const result = await fetchData()
 
   return (
     <>
       <div className="p-4 py-6 mb-4 flex justify-center items-center bg-chart overflow-hidden">
         전체반복 ON / 랜덤재생 OFF / 중복곡 허용 / 캐싱적용 OFF
       </div>
-      {result.agreegation.map((e, index) => (
+      {result && result.agreegation.map((e, index) => (
         <div key={index}>
           <div className="mx-6 mt-4 mb-2">
             {convertRedableChartType(e.type)}
@@ -47,3 +34,5 @@ export default async function ChartPage() {
     </>
   );
 }
+
+export const dynamic = 'force-dynamic'
