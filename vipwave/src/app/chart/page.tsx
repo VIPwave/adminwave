45f969dd29afbb0b8c469737db74f5cdd8d5e6e8
gv0413 <ggvv0413@gmail.com>
2025-02-23 09:24:09 +0900

    chart) 데이터 없을 경우 예외처리

--- vipwave/src/app/chart/page.tsx
+++ vipwave/src/app/chart/page.tsx
@@ -33,17 +33,23 @@ export default async function ChartPage() {
       <div className="p-4 py-6 mb-4 flex justify-center items-center bg-chart overflow-hidden">
         전체반복 ON / 랜덤재생 OFF / 중복곡 허용 / 캐싱적용 OFF
       </div>
-      {result.agreegation.map((e, index) => (
-        <div key={index}>
-          <div className="mx-6 mt-4 mb-2">
-            {convertRedableChartType(e.type)}
-            <span className="float-right text-xs text-gray-500">
-              {e.timestamp}
-            </span>
-          </div>
-          <Charts items={e.items} />
+      {!result?.agreegation && (
+        <div className="flex justify-center items-center h-[300px]">
+          <span>데이터가 없습니다.</span>
         </div>
-      ))}
+      )}
+      {result &&
+        result.agreegation.map((e, index) => (
+          <div key={index}>
+            <div className="mx-6 mt-4 mb-2">
+              {convertRedableChartType(e.type)}
+              <span className="float-right text-xs text-gray-500">
+                {e.timestamp}
+              </span>
+            </div>
+            <Charts items={e.items} />
+          </div>
+        ))}
     </>
   );
 }
