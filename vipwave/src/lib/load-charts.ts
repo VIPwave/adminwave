c83089695561d5419529f2bc74e9df77b7b811fa
dino <youheedong@gmail.com>
2025-02-24 15:55:56 +0900

    fix: singleton 적용

--- vipwave/src/lib/load-charts.ts
+++ vipwave/src/lib/load-charts.ts
@@ -1,12 +1,10 @@
-// The following function is shared
-// with getStaticProps and API routes
 import { MongoClient } from "mongodb";
 import { Song } from "./components/chart/ChartTable";
 
 export type ChartAgreegation = {
     agreegation: Array<ChartItem>;
 };
-  
+
 type ChartItem = {
     platform: string;
     type: string;
@@ -14,19 +12,26 @@ type ChartItem = {
     timestamp: string;
 };
 
-// from a `lib/` directory
+let client: MongoClient | null = null;
+
+async function getMongoClient() {
+    if (!client) {
+        client = new MongoClient(
+          "mongodb+srv://service_readonly_db_user:8sGfklmdfABM72Cd@vipwave.lruc9.mongodb.net/?retryWrites=true&w=majority&appName=vipwave"
+        );
+        await client.connect();
+    }
+    return client;
+}
+
 export async function loadPosts() {
-    const client = new MongoClient(
-        "mongodb+srv://service_readonly_db_user:8sGfklmdfABM72Cd@cluster0.lruc9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    );
-    await client.connect();
+    const client = await getMongoClient();
     const db = client.db("music_charts");
     const collection = db.collection("charts");
-    // NOTE: mongoDB의 Data이기 때문에, 임의로 캐스팅함.
     try {
-        return (await collection.findOne({id: "vip-wave"})) as unknown as ChartAgreegation || { agreegation: [] };
-    } catch(e) {
-        console.log("e =", e)
+        return (await collection.findOne({ id: "vip-wave" })) as unknown as ChartAgreegation || { agreegation: [] };
+    } catch (e) {
+        console.log("e =", e);
         return { agreegation: [] };
     }
 }
