4eab098bb68fcf30f542fa1c321a78809b8be4bf
dino.22 <youheedong@gmail.com>
2025-02-24 22:27:08 +0900

    fix: metadata

--- vipwave/src/app/layout.tsx
+++ vipwave/src/app/layout.tsx
@@ -3,6 +3,14 @@ import "./globals.css";
 import Header from "@/lib/components/navigation/Header";
 import Footer from "@/lib/components/footer/Footer";
 import Head from "next/head";
+import { Metadata } from "next";
+
+// metadata
+export const metadata: Metadata = {
+  title: 'VIPWAVE',
+  description: '빅뱅 음원총공팀',
+  icons: ["icon.ico"],
+};
 
 export default function RootLayout({
   children,
